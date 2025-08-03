ff8f6d3e913909134dfc285522aa76fc64614ed2
freedomkwok <freedomkwok@hotmail.com>
2025-08-03 15:48:57 -0700

    ADD auto pull

--- ROOT/opt/instance-tools/bin/entrypoint.sh
+++ ROOT/opt/instance-tools/bin/entrypoint.sh
@@ -350,6 +350,8 @@ sync_environment() {
 
     rm -rf /.uv >/dev/null 2>&1
     ln -s "${uv_dir}" /.uv
+
+    cd /workspace/OpenRL && git remote add origin https://$GH_TOKEN@github.com/freedomkwok/OpenRL.git && git pull origin main
 }
 
 main "$@"
